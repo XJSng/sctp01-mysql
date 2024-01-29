cb5d3a6943849067895174dc3af97f7317805f69
Xu Jie Sng <sngxujie@gmail.com>
2024-01-29 08:37:36 +0000

    update CR forms for MySQL and express

--- 03-mysql-express/index.js
+++ 03-mysql-express/index.js
@@ -26,30 +26,54 @@ async function main() {
         user: process.env.DB_USER,
         database: process.env.DB_DATABASE,
         password: process.env.DB_PASSWORD
-
-
     })
 
-
-    app.get('/customers', async function(req, res) {
-        const [customers] = await connection.execute("SELECT * FROM Customers")
+    // GET ROUTES
+    app.get('/customers', async function (req, res) {
+        // We want the first element from the array return from connection.execute
+        const [customers] = await connection.execute(
+            `SELECT * FROM Customers
+            JOIN Companies ON Customers.company_id = Companies.company_id;
+            `);
         // This [customer] is known as array destructing
         // this is the same as 
-        // const custmer = await connection.execute("SELECT * FROM customers")[0]
-        
-
+        // const customer = await connection.execute("SELECT * FROM customers")[0]
         // Render using Handlebars template
         res.render('customers/index', {
             customers
         });
     });
 
-    app.get("/customers/create", async function(req, req){
-        res.render('customers/create')
+    // POST ROUTES
+    //Create customer form
+    app.get("/customers/create", async (req, res) => {
+        const [companies] = await connection.execute(`
+    SELECT * FROM Companies`)
+        res.render('customers/create', {
+            companies
+        })
     })
+
+    // POST customer form
+    app.post("/customers/create", async (req, res) => {
+        const { first_name, last_name, rating, company_id } = req.body
+        const query = `
+        INSERT INTO Customers (first_name, last_name, rating, company_id)
+    VALUES ('${first_name}', '${last_name}', ${rating}, ${company_id})
+        `
+        await connection.execute(query);
+    res.redirect('/customers')
+
+    })
+    
+    // UPDATE ROUTES
+
+    // DELETE ROUTES
+
+    // END OF ROUTES
 }
 main()
 
-app.listen(3000, ()=>{
+app.listen(3000, () => {
     console.log("server has started")
 })
